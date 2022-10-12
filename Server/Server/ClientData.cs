a7f00c14b8454f2edac93165cebd967a4e1c9c32
ycs-201807006 <80028759+ycs-201807006@users.noreply.github.com>
2022-10-12 23:20:05 +0900

    Add files via upload

---
+++ Server/Server/ClientData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class ClientData
+    {
+
+
+        public ClientData(TcpClient client)
+        {
+
+        }
+
+    }
+}
