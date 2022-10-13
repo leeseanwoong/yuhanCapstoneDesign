546a389fb385e58ca24f730b21f599dd3c22ea87
ycs-201807006 <80028759+ycs-201807006@users.noreply.github.com>
2022-10-13 21:13:36 +0900

    Add files via upload

---
+++ Client/321/소스.cpp
@@ -0,0 +1,165 @@
+#pragma comment(lib, "ws2_32") // 소켓을 사용하기 위해서 라이브러리 참조해야 한다.
+#pragma warning(disable:4996) // inet_ntoa가 deprecated가 되었는데.. 사용하려면 아래 설정을 해야 한다.
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <WinSock2.h> // 소켓을 사용하기 위한 라이브러리
+#include <string>
+#include <cctype>
+#define BUFFERSIZE 64 // 수신 버퍼 사이즈
+using namespace std;
+// 콘솔에 메시지를 출력하는 함수
+
+
+/*void print(vector<wchar_t>* str)
+{
+    
+    int p = 0; // 포인트 위치
+    
+    wchar_t out[BUFFERSIZE + 2];// 버퍼 설정. +1은 \0를 넣기 위한 크기
+    
+    cout << "From server message : ";// 콘솔 출력
+
+    for (int n = 0; n < (str->size() / BUFFERSIZE) + 1; n++)
+    {
+        
+        int size = str->size();// 버퍼 사이즈 설정
+        
+        if (size > BUFFERSIZE) {// 수신 데이터가 버퍼 사이즈를 넘었을 경우.
+            if (str->size() < (n + 1) * BUFFERSIZE)
+            {
+                size = str->size() % BUFFERSIZE;
+            }
+            else
+            {
+                size = BUFFERSIZE;
+            }
+        }
+        
+        for (int i = 0; i < size; i++, p++)// echo 메시지와 콘솔 메시지를 작성한다.
+        {
+            out[i] = *(str->begin() + p);
+        }
+        
+        wprintf(L"%s", out);// 콘솔 메시지 콘솔 출력. //cout << out;
+        
+    }
+}*/
+void firstsend(const wchar_t* userinfo, SOCKET sock)
+{
+    wprintf_s(userinfo);
+    send(sock, (char*)userinfo, (wcslen(userinfo) * 2), 0);
+    char cBuffer[BUFFERSIZE] = {};
+    recv(sock, cBuffer, BUFFERSIZE, 0);
+}
+
+
+// 실행 함수
+int main() 
+{
+    int i = 0;
+
+    const wchar_t * UserInfo = L"8#201807006#이석종"; // 클라이언트 학번
+    
+    //string * Msg = &hak; // 서버에 보내줄때에 사용
+    WSADATA wsaData; // 소켓 정보 데이터 설정
+    
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // 소켓 실행.
+    {
+        return 1;
+    }
+
+    SOCKET sock = socket(PF_INET, SOCK_STREAM, 0); // Internet의 Stream 방식으로 소켓 생성 
+
+    SOCKADDR_IN addr; // 소켓 주소 설정
+
+    memset(&addr, 0, sizeof(addr)); // 구조체 초기화
+    addr.sin_family = AF_INET; // 소켓은 Internet 타입
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 127.0.0.1(localhost)로 접속하기
+    addr.sin_port = htons(9000); // 포트 8898으로 접속
+
+
+    if (connect(sock, (SOCKADDR*)&addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) // 접속
+    {
+        cout << "error" << endl; // 에러 콘솔 출력
+        return 1;
+    }
+
+    
+
+    firstsend(UserInfo, sock);
+
+    char cMsg[1024] = ""; // 메시지 버퍼
+
+   
+
+    while (1) {
+
+        cout << "입력" << endl;
+        cin >> cMsg;
+
+        printf("%d - 내가 보내는 메시지 : -%s-\n", i, cMsg);
+        send(sock, cMsg, strlen(cMsg), 0);
+
+        char cBuffer[BUFFERSIZE] = {};
+        recv(sock, cBuffer, BUFFERSIZE, 0);
+        printf("%d - 서버에서 온 메시지 : -%s-\n",i++, cBuffer);
+
+        if (cMsg[0] == '9' && cMsg[1] == '\0') { 
+            break;
+        }  // 클라이언트 나감 처리
+    }
+    closesocket(sock); // 서버 소켓 종료
+    WSACleanup(); // 소켓 종료
+    return 0;
+}
+
+
+
+/*
+   vector<wchar_t> buffer; // telent은 한글자씩 데이터가 오기 때문에 글자를 모을 buffer가 필요하다.(wchar_t 타입으로 선언)
+   wchar_t x; // 수신 데이터
+   while (1)
+   {
+       // 데이터를 받는다. 에러가 발생하면 멈춘다.
+       // char* 형식으로 받기 때문에 타입 캐스팅을 한다.
+       if (recv(sock, (char*)&x, sizeof(x), 0) == SOCKET_ERROR)
+       {
+           cout << "error" << endl; // 에러 콘솔 출력
+           break;
+       }
+       buffer.push_back(x); // 버퍼에 글자를 하나 넣는다.
+       if (buffer.size() > 4 && *(buffer.end() - 4) == '\r' && *(buffer.end() - 3) == '\n' && *(buffer.end() - 2) == '>' && *(buffer.end() - 1) == '\0') // \r\n>\0가 나오면 콘솔에 출력하고 콘솔로 부터 메시지를 기다린다.
+       {
+
+
+
+           buffer.clear(); // 버퍼 초기화
+
+           char input[BUFFERSIZE]; // 콘솔로 부터 입력을 받는다.
+
+           cin >> input; // 유저로 부터 입력 받기
+
+           int size = strlen(input); // 입력받은 길이를 받는다.
+
+           wchar_t buffer[BUFFERSIZE * 2]; // 개행을 넣는다.
+
+           mbstowcs(&buffer[0], input, BUFFERSIZE * 2); // char*에서 wchar_t*으로 변환하는 함수
+           *(buffer + size) = '\r';
+           *(buffer + size + 1) = '\n';
+           *(buffer + size + 2) = '\0';
+           // 서버로 보내기
+           // send함수가 char* 형식으로 보낼 수 있기 때문에 타입 캐스팅을 한다.
+           send(sock, (char*)buffer, wcslen(buffer) * 2, 0);
+
+           if (*input == 'e' && *(input + 1) == 'x' && *(input + 2) == 'i' && *(input + 3) == 't') // 메시지가 exit라면 종료
+           {
+               break;
+           }
+           else if (*input == 'a' && *(input + 1) == 'a' && *(input + 2) == 'a' && *(input + 3) == 'a') {
+               cout << "aa";
+           }
+           continue;
+       }
+   }*/
---
+++ Server/Server/MyServer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections.Concurrent;
+
+namespace Server
+{
+    class UserInfo
+    {
+        public string hak { get; set; }
+        public string name { get; set; }
+    }
+
+    class MyServer
+    {
+        private int m_cmd;  // 종류
+
+        public static readonly int cmd_ClientFileSendRequest = 7;
+        public static readonly int cmd_Login = 8;
+        public static readonly int cmd_exit = 9;
+
+
+        public TcpListener server { get; set; }
+
+        // server를 실행시킬 스레드    
+        private Thread server_tr, msg_tr, all_tr;
+
+        // 서버에 연결되어 있는 클라이언트 수 만 됨(정보도 받아와야함)
+        public ConcurrentDictionary<TcpClient, UserInfo> connectedClients = new ConcurrentDictionary<TcpClient, UserInfo>();
+
+        string data = null;
+
+        public MyServer() // 생성자
+        {
+            // TcpListener 클래스 생성 ( IP, 포트 )
+            server = new TcpListener(System.Net.IPAddress.Any, 9000);
+
+            server_tr = new Thread(WaitAndAcceptClient);
+            server_tr.IsBackground = true;
+        }
+
+        ~MyServer() // 소멸자
+        {
+            server.Stop();
+            server_tr.Interrupt();
+        }
+
+        public void Start() // 시작
+        {
+            // 서버 시작
+            server.Start();
+            server_tr.Start();
+            Console.WriteLine("서버켜짐");
+        }
+
+        private void WaitAndAcceptClient() // 클라이언트 응답 요청
+        {
+            try
+            {
+                while (true)
+                {
+                    // 서버에 접속하려는 client 접속요청 허용후 클라이언트 객체에 할당
+                    // 접속하려는 client가 없으면 무한 대기
+                    // 서버에 접속하면 접속한 IP 출력
+                    TcpClient AcceptClient = server.AcceptTcpClient();
+                    Console.WriteLine("클라이언트 접속: {0} ", ((IPEndPoint)AcceptClient.Client.RemoteEndPoint).ToString());
+
+                    ClientData clientData = new ClientData(AcceptClient);
+
+                    msg_tr = new Thread(new ParameterizedThreadStart(Msg));
+                    msg_tr.Start(AcceptClient);
+                    /*all_tr = new Thread(new ParameterizedThreadStart(allSendMsg));
+                    all_tr.Start(AcceptClient); */// 전체 보내기 기능 일단 죽여놓음
+                }
+            }
+            catch (ThreadStateException)
+            {
+                return;
+            }
+        }
+
+        private void Msg(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj; // 스레드 에서 받아온 tcpclient 정보
+            NetworkStream stream = tcpClient.GetStream(); // 스트림
+            int length; // 길이
+            byte[] bytes = new byte[256]; // 버퍼
+            string[] TempData;
+            int i = 0 , k = 0;
+
+            try
+            {
+                while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    if (k == 1) {
+                        data = Encoding.Default.GetString(bytes, 0, length);
+                        Console.WriteLine(String.Format("수신: {0}", data));
+                        byte[] msg = Encoding.Default.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine(String.Format("송신: {0}", data));
+                        ShowconnectUser();
+                    } else {
+                        for (; i < 1; i++)
+                        {
+                            data = Encoding.Unicode.GetString(bytes, 0, length + 2);
+                            Console.WriteLine(String.Format("수신: {0}", data));
+                            byte[] msg1 = Encoding.Unicode.GetBytes(data);
+                            stream.Write(msg1, 0, msg1.Length);
+                            Console.WriteLine(String.Format("송신: {0}", data));
+                            k = 1;
+                        }
+                    }
+
+                    TempData = data.Split("#");
+                    m_cmd = int.Parse(TempData[0]);
+
+                    // Protocol
+                    if (m_cmd == cmd_exit) {
+                        ClientExit(tcpClient);
+                    }
+                    else if (m_cmd == cmd_Login) {
+                        ClientLogin(tcpClient, TempData);
+                    }
+                    else if (m_cmd == 2) {
+
+                    }
+                }
+            }
+            catch (Exception e) {
+                tcpClient.Close();
+            }
+        }
+        private void allSendMsg(object obj) 
+        {
+            foreach (var display in connectedClients)
+            {
+                NetworkStream stream = display.Key.GetStream(); // 보낼 유저의 스트림 가져오기
+                byte[] msg = Encoding.Default.GetBytes(display.Value.name); // 보낼값
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine(String.Format("{0}", data)); // 보낸값 출력
+            }
+        } // 현재 접속해 있는 모든 유저에게 메시지 전송
+        private string Left(string str, int Length) 
+        {
+            if (str.Length < Length)
+                Length = str.Length;
+
+            return str.Substring(0, Length);
+        } // left 함수 구현 미사용
+        private void ShowconnectUser() 
+        {
+            Console.WriteLine("현재 접속해 있는 클라이언트 수 : " + connectedClients.Count());
+            foreach (var display in connectedClients)
+            {
+                Console.Write(display.Value.hak + "#");
+                Console.WriteLine(display.Value.name);
+            }
+        } // 현재 접속해 있는 모든 유저의 학번과 이름 출력
+        private void ClientExit(TcpClient tcpClient)
+        {
+            Console.WriteLine("클라이언트 종료: {0} ", ((IPEndPoint)tcpClient.Client.RemoteEndPoint).ToString());
+            for (int j = 0; j < connectedClients.Count(); j++)
+            {
+                connectedClients.TryRemove(tcpClient, out _);
+            }
+            tcpClient.Close();
+            ShowconnectUser();
+        }  // 나감 처리
+        private void ClientLogin(TcpClient tcpClient, string[] tempdata)
+        {
+            UserInfo userinfo = new UserInfo();
+            userinfo.hak = tempdata[1];
+            userinfo.name = tempdata[2];
+            connectedClients.TryAdd(tcpClient, userinfo);
+            ShowconnectUser();
+        }  // 클라이언트 로그인 햇을때에 해당 유저의 정보를 서버 배열에 저장되는 구조
+    }
+}
