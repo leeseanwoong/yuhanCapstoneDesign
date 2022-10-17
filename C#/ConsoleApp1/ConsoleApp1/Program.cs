20ab90fcb45beaa9e8a11b87dfb8136c10697684
Jominwoo0220 <80020103+jominwoo10@users.noreply.github.com>
2022-10-17 22:32:12 +0900

    Add files via upload

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TestWeb;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //압축
+            ZipManager.ZipFiles(@"D:\ZipTest\unzipFolder\", @"C:\Users\YUHAN\Desktop\ZipFile\Video.zip", "1234", false);
+            //압축해제
+            ZipManager.UnZipFiles(@"C:\Users\YUHAN\Desktop\ZipFile\Video.zip", @"D:\ZipTest\unzipFolder\", "1234", false);
+        }
+    }
+}
